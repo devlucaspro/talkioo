05c37bdc8d9b4d96e2dbe31f67f5dc5b318d4a0b
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2023-05-31 15:44:20 -0300

    :recycle: refactor: Arrumando menu mobile e rotas privadas

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -26,18 +26,28 @@ const Header = () => {
       menu.classList.remove('menu-active');
       document.body.classList.remove('overflow');
     });
+
+    let liItems = document.querySelectorAll('#menu li')
+    liItems.forEach((item) => {
+      item.addEventListener('click', () => {
+        document.body.classList.remove('overflow');
+        divI.classList.remove('div-visible');
+        menu.classList.remove('menu-active');
+      })
+    })
   };
 
   return (
     <header>
-      <img className='logo' src="./assets/logo.png" alt="Talkioo"/>
-      <button className='btn-menu' onClick={openMenu}>Menu <i className="fa-solid fa-chevron-down"></i></button>
+      <img className='logo' src="./assets/logo.png" alt="Talkioo" />
+      {user ? true : <button className='btn-menu' id='btn' onClick={openMenu}>Menu <i className="fa-solid fa-chevron-down"></i></button>}
       <ul id='menu'>
         {user ? true : <li><Link to='/'>Home</Link></li>}
         <li><Link to='/lessons'>Lessons <i className="fa-solid fa-paperclip fa-sm"></i></Link></li>
         {user && <li><Link to='/blog'>Blog</Link></li>}
       </ul>
       {user ? <button className='btn-bell'><i className="fa-solid fa-bell fa-xl"></i></button> : <Btn to='/login'>Login <i className="fa-solid fa-right-to-bracket fa-sm"></i></Btn>}
+      {user ? <button className='btn-menu' id='btn2' onClick={openMenu}>Menu <i className="fa-solid fa-chevron-down"></i></button> : true}
 
       <div id='invisible-div'>
         .
--- src/routes/Private.jsx
+++ src/routes/Private.jsx
@@ -1,10 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Navigate } from 'react-router-dom';
 import { appContext } from '../contexts/auth';
 
 const Private = ({ children, redirectPath = '/' }) => {
 
-  const {user} = useContext(appContext);
+  const {user, setUser} = useContext(appContext);
+
+  useEffect(() => {
+    function loadPage() {
+      if(user) {
+        let storage = localStorage.getItem('userToken');
+        if(storage) {
+          setUser(JSON.parse(storage));
+        }
+      }
+    }
+
+    loadPage()
+  }, [])
 
   if(!user) {
     return <Navigate to={redirectPath} replace />
